9fbf601bbfa555fd8fc8054465583341002bea30
arihant <2016047@iiitdmj.ac.in>
2017-06-23 12:49:13 +0530

    Update submit1.js

--- submit1.js
+++ submit1.js
@@ -1,4 +1,9 @@
-var res="",r;
+function cdf()
+{
+	var y = document.getElementById("x1");
+	y.value = "";
+}
+	
 function cal1(x)
 {
 	var y = document.getElementById("x1");
@@ -10,9 +15,24 @@ function sum()
 	var y = document.getElementById("x1");
 	y.value+="+";
 }
+function sub()
+{
+	var y = document.getElementById("x1");
+	y.value+="-";
+}
+function mul()
+{
+	var y = document.getElementById("x1");
+	y.value+="*";
+}
+function div()
+{
+	var y = document.getElementById("x1");
+	y.value+="/";
+}
 function cal2()
 {
 	var y = document.getElementById("x1");
 	y.value = eval(y.value);
 }	
-	
+
